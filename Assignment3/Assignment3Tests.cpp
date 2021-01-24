ce8e7ff98c52723ed7f36acbf7384a94c373f406
Stephe00 <45608529+stephe00@users.noreply.github.com>
2021-01-23 19:54:32 -0800

    Add files via upload

---
+++ Assignment3/Assignment3Tests.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+// Assignment 3 Tests
+
+bool isempty()
+{
+    if(front == NULL && rear = NULL)
+    return true;
+    else
+    {
+      return false; 
+    }
+    
+}
+
+// tests 
+
+int test()
+{
+    if (isempty('front = NULL && rear = NULL') != true) return 1;
+    if (isempty('front == 0> && rear = 0>') != false) return 2;
+ 
+    return 0;
+}
+ 
+int main()
+{
+    return 0;
+}
