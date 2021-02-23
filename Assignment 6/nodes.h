12363ce8666077f02941e427335ce2c26361b167
Stephe00 <45608529+stephe00@users.noreply.github.com>
2021-02-22 18:58:17 -0800

    Create nodes.h
    helper file

---
+++ Assignment 6/nodes.h
@@ -0,0 +1,13 @@
+// node.h
+// assignment 6 
+// binary search tree 
+// 2.20.2021
+// stephen oium 
+
+// struct 
+struct node { // node 
+    int value; // value 
+    node *left; // less than 
+    node *right; // > greater than 
+};
+
